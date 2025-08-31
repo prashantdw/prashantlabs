6d1ca89e67e8335e79d93d758989a53735629030
prashantdw <33394423+prashantdw@users.noreply.github.com>
2025-08-31 14:19:20 +0530

    Making Microservices Resilient using Resilience4j

---
+++ microservices/section_10/accounts/src/main/java/com/prashantlabs/accounts/AccountsApplication.java
@@ -0,0 +1,18 @@
+package com.prashantlabs.accounts;
+
+import com.prashantlabs.accounts.dto.AccountsContactInfoDto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
